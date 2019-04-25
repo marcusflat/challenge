76393db63f9fbeb1f9b13f912450c0534d189412
Marcus Lima <marcus_fonseca@ymail.com>
2019-04-25 19:55:31 -0300

    Personalização da scroll bar e ponta triagular do popover

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 const cart = document.querySelector("#shopping-cart");
 const miniCart = document.querySelector("#mini-cart");
+const triangle = document.querySelector("#arrow-up")
 
 cart.addEventListener("click", () => {
-  console.log("Carrinho clicado");
   const miniCartDisplay = getComputedStyle(miniCart).display;
-  miniCartDisplay == "block"
-    ? (miniCart.style.display = "none")
-    : (miniCart.style.display = "block");
+  if (miniCartDisplay == "block") {
+    miniCart.style.display = "none";
+    triangle.style.display = "none";
+  } else {
+    miniCart.style.display = "block";
+    triangle.style.display = "block";
+  }
 });
