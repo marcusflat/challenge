f6fd2c25a299403d5ba2b3b09d37667606eb6d4a
Marcus Lima <marcus_fonseca@ymail.com>
2019-04-26 01:13:47 -0300

    refatoração código toggleMiniCart

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 const cart = document.querySelector("#shopping-cart");
-const miniCart = document.querySelector("#mini-cart");
-const triangle = document.querySelector("#arrow-up")
 
 cart.addEventListener("click", () => {
+  toggleMiniCart();
+});
+
+
+function toggleMiniCart() {
+  const miniCart = document.querySelector("#mini-cart");
+  const triangle = document.querySelector("#arrow-up");
   const miniCartDisplay = getComputedStyle(miniCart).display;
+
   if (miniCartDisplay == "block") {
     miniCart.style.display = "none";
     triangle.style.display = "none";
@@ -11,4 +17,4 @@ cart.addEventListener("click", () => {
     miniCart.style.display = "block";
     triangle.style.display = "block";
   }
-});
+}
